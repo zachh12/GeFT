56dae96dc09ff04111e7c6bdc408d1fa1f19ca67
zachh12 <zhainsel@gmail.com>
2019-05-29 10:27:46 -0400

    Updated setup

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#Environment Variable setup for waveform fitting
+
+export DATADIR=/Data
+export WAFFLEDIR=../Software/waffle
+export PYGAMADIR=../Software/pygama
+
+#Setup appropriate python environment in order to have
+#needed packages available for computation
+
+#PYTHONPATH=../anaconda3/bin
